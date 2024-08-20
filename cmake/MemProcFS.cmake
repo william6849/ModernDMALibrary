48c1a55f9eaecf7b790f486e63649f43f8c09d5e
zznzm <william6849@gmail.com>
2024-08-20 16:01:47 +0800

    update Leech version

--- cmake/MemProcFS.cmake
+++ cmake/MemProcFS.cmake
@@ -4,18 +4,18 @@ set_directory_properties(PROPERTIES EP_BASE ${CMAKE_BINARY_DIR}/third_party)
 
 if(WIN32)
   set(RELEASE_URL
-      https://github.com/ufrisk/MemProcFS/releases/download/v5.11/MemProcFS_files_and_binaries_v5.11.4-win_x64-20240814.zip
+      https://github.com/ufrisk/MemProcFS/releases/download/v5.11/MemProcFS_files_and_binaries_v5.11.4-win_x64-20240817.zip
   )
 else()
   if(CMAKE_SYSTEM_PROCESSOR MATCHES "aarch64")
     set(RELEASE_URL
-        https://github.com/ufrisk/MemProcFS/releases/download/v5.11/MemProcFS_files_and_binaries_v5.11.4-linux_aarch64-20240814.tar.gz
+        https://github.com/ufrisk/MemProcFS/releases/download/v5.11/MemProcFS_files_and_binaries_v5.11.4-linux_aarch64-20240817.tar.gz
     )
   elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "i386")
     message(WARNING "x32 platform is not support currently.")
   elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "x86_64")
     set(RELEASE_URL
-        https://github.com/ufrisk/MemProcFS/releases/download/v5.11/MemProcFS_files_and_binaries_v5.11.4-linux_x64-20240814.tar.gz
+        https://github.com/ufrisk/MemProcFS/releases/download/v5.11/MemProcFS_files_and_binaries_v5.11.4-linux_x64-20240817.tar.gz
     )
   endif()
 endif()
