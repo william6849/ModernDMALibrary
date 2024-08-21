1247576b3195eb9a1554a5fd1ade010adff4a336
zznzm <william6849@gmail.com>
2024-08-21 16:53:55 +0800

    Add handler RAII

---
+++ MordenDMALibrary/src/leech_wrapper.cc
@@ -0,0 +1,28 @@
+#include "leech_wrapper.h"
+
+#include "spdlog/spdlog.h"
+
+void VMMHandleDeleter::operator()(VMM_HANDLE handle) const {
+  if (handle) {
+    spdlog::debug("Closing VMM handler: {}", static_cast<void*>(handle));
+    VMMDLL_Close(handle);
+  }
+}
+
+VMMHandleWrapper::VMMHandleWrapper(VMM_HANDLE handle) : handle_(handle) {}
+
+VMMHandleWrapper::VMMHandleWrapper(VMMHandleWrapper&& other) noexcept
+    : handle_(std::move(other.handle_)) {}
+
+VMMHandleWrapper& VMMHandleWrapper::operator=(
+    VMMHandleWrapper&& other) noexcept {
+  if (this != &other) {
+    handle_ = std::move(other.handle_);
+    other.handle_ = nullptr;
+  }
+  return *this;
+}
+
+VMM_HANDLE VMMHandleWrapper::get() const { return handle_.get(); }
+
+VMMHandleWrapper::operator VMM_HANDLE() const { return handle_.get(); }
