00edcb3fae416b659cce991b907aea2249b47dbb
zznzm <william6849@gmail.com>
2024-10-03 05:04:07 +0800

    Auto grab latest leech

--- cmake/LeechCore.cmake
+++ cmake/LeechCore.cmake
@@ -2,25 +2,31 @@ include(ExternalProject)
 
 set_directory_properties(PROPERTIES EP_BASE ${CMAKE_BINARY_DIR}/third_party)
 
+execute_process(
+  COMMAND curl -s https://api.github.com/repos/ufrisk/LeechCore/releases/latest
+  OUTPUT_VARIABLE RELEASE_JSON)
+
 if(WIN32)
-  set(RELEASE_URL
-        https://github.com/ufrisk/LeechCore/releases/download/v2.19/LeechCore_files_and_binaries_v2.19.1-win_x64-20240930.zip
-  )
+  set(OS_TYPE win_x64)
 else()
   if(CMAKE_SYSTEM_PROCESSOR MATCHES "aarch64")
-    set(RELEASE_URL
-        https://github.com/ufrisk/LeechCore/releases/download/v2.19/LeechCore_files_and_binaries_v2.19.1-linux_aarch64-20240930.tar.gz
-    )
+    set(OS_TYPE aarch64)
   elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "i386")
     message(WARNING "x32 platform is not support currently.")
   elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "x86_64")
-    set(RELEASE_URL
-        https://github.com/ufrisk/LeechCore/releases/download/v2.19/LeechCore_files_and_binaries_v2.19.1-linux_x64-20240930.tar.gz
-    )
+    set(OS_TYPE linux_x64)
   endif()
 endif()
 
-message("Fetch LeechCore Release")
+string(REGEX MATCH "\"browser_download_url\": \"([^\"]*${OS_TYPE}[^\"]*)\"" _
+             "${RELEASE_JSON}")
+set(RELEASE_URL ${CMAKE_MATCH_1})
+string(REGEX MATCH "(v[0-9]+\.[0-9]+\.[0-9]+)" RELEASE_VERSION "${RELEASE_URL}")
+string(REGEX MATCH "-([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])." _
+             "${RELEASE_URL}")
+set(RELEASE_DATE ${CMAKE_MATCH_1})
+message("Fetch Latest LeechCore Release: " ${RELEASE_VERSION} " LastUpdate: "
+        ${RELEASE_DATE})
 message(VERBOSE "Target Release: " ${RELEASE_URL})
 
 ExternalProject_Add(
@@ -32,11 +38,11 @@ ExternalProject_Add(
 
 ExternalProject_Get_Property(LeechCore SOURCE_DIR)
 set(LEECHCORE_RESOURCE_DIR ${SOURCE_DIR})
-set_target_properties(LeechCore PROPERTIES
-    BUILD_RPATH "."
-    INSTALL_RPATH "."
-    INSTALL_RPATH_USE_LINK_PATH TRUE
-)
+set_target_properties(
+  LeechCore
+  PROPERTIES BUILD_RPATH "."
+             INSTALL_RPATH "."
+             INSTALL_RPATH_USE_LINK_PATH TRUE)
 
 add_library(leechcorelib SHARED IMPORTED GLOBAL)
 add_dependencies(leechcorelib LeechCore)
--- cmake/MemProcFS.cmake
+++ cmake/MemProcFS.cmake
@@ -2,25 +2,31 @@ include(ExternalProject)
 
 set_directory_properties(PROPERTIES EP_BASE ${CMAKE_BINARY_DIR}/third_party)
 
+execute_process(
+  COMMAND curl -s https://api.github.com/repos/ufrisk/MemProcFS/releases/latest
+  OUTPUT_VARIABLE RELEASE_JSON)
+
 if(WIN32)
-  set(RELEASE_URL
-        https://github.com/ufrisk/MemProcFS/releases/download/v5.11/MemProcFS_files_and_binaries_v5.11.6-win_x64-20240928.zip
-  )
+  set(OS_TYPE win_x64)
 else()
   if(CMAKE_SYSTEM_PROCESSOR MATCHES "aarch64")
-    set(RELEASE_URL
-        https://github.com/ufrisk/MemProcFS/releases/download/v5.11/MemProcFS_files_and_binaries_v5.11.6-linux_aarch64-20240928.tar.gz
-    )
+    set(OS_TYPE aarch64)
   elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "i386")
     message(WARNING "x32 platform is not support currently.")
   elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "x86_64")
-    set(RELEASE_URL
-        https://github.com/ufrisk/MemProcFS/releases/download/v5.11/MemProcFS_files_and_binaries_v5.11.6-linux_x64-20240928.tar.gz
-    )
+    set(OS_TYPE linux_x64)
   endif()
 endif()
 
-message("Fetch MemProcFS Release")
+string(REGEX MATCH "\"browser_download_url\": \"([^\"]*${OS_TYPE}[^\"]*)\"" _
+             "${RELEASE_JSON}")
+set(RELEASE_URL ${CMAKE_MATCH_1})
+string(REGEX MATCH "(v[0-9]+\.[0-9]+\.[0-9]+)" RELEASE_VERSION "${RELEASE_URL}")
+string(REGEX MATCH "-([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])." _
+             "${RELEASE_URL}")
+set(RELEASE_DATE ${CMAKE_MATCH_1})
+message("Fetch Latest MemProcFS Release: " ${RELEASE_VERSION} " LastUpdate: "
+        ${RELEASE_DATE})
 message(VERBOSE "Target Release: " ${RELEASE_URL})
 
 ExternalProject_Add(
@@ -32,11 +38,11 @@ ExternalProject_Add(
 
 ExternalProject_Get_Property(MemProcFS SOURCE_DIR)
 set(MEMPROCFS_RESOURCE_DIR ${SOURCE_DIR})
-set_target_properties(MemProcFS PROPERTIES
-    BUILD_RPATH "."
-    INSTALL_RPATH "."
-    INSTALL_RPATH_USE_LINK_PATH TRUE
-)
+set_target_properties(
+  MemProcFS
+  PROPERTIES BUILD_RPATH "."
+             INSTALL_RPATH "."
+             INSTALL_RPATH_USE_LINK_PATH TRUE)
 
 add_library(memprocfslib SHARED IMPORTED GLOBAL)
 add_dependencies(memprocfslib MemProcFS)
